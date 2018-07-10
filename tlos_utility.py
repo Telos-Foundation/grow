cebe47d4a36e689c3120b3acc6403da4b2bf8091
ARoomWithABue <peter@hornofthemoon.com>
2018-07-10 12:57:50 -0700

    Initial commit with the code moved from EOS-NODE

---
+++ tlos_utility.py
@@ -0,0 +1,74 @@
+import json
+import os
+import subprocess
+import time
+import sys
+import random
+
+
+def jsonArg(a):
+    return " '" + json.dumps(a) + "' "
+
+def file_get_contents(filename):
+    with open(filename) as f:
+        return f.read()
+
+def log_file(filename):
+    f = open(filename, 'a')
+    f.seek( 0, os.SEEK_END )
+    return f
+
+def create_file(name, input):
+    f = open(name, "w")
+    f.write(str(input))
+    f.close()
+
+def delete_file(file):
+    os.remove(file)    
+
+def run(args, isShell = True):
+    print(args)
+    if subprocess.call(args, shell=isShell):
+        print('exiting because of error')
+        sys.exit(1)
+
+def run_retry(args, isShell = True, num = 1):
+    print(args)
+    o = subprocess.call(args, shell=isShell)
+    if o and num <= 3:
+        print('Failed retrying... ')
+        time.sleep(5)
+        run_retry(args, True, num + 1)
+    elif o:
+        print('exiting because of error')
+        sys.exit(1)
+
+def run_continue(args, isShell = True):
+    print(args)
+    if subprocess.call(args, shell=isShell):
+        print('command failed')
+
+def get_output(args):
+    print args
+    proc = subprocess.Popen(args, shell=True, stdout=subprocess.PIPE)
+    return proc.communicate()[0].decode('utf-8')
+
+def start_background_proc(args, file):
+    print args
+    p = subprocess.Popen(args,
+        stdout=file,
+        stderr=file, shell=True)
+    create_file("nodeos.pid", p.pid)
+
+def background(args):
+    return subprocess.Popen(args, shell=True)
+
+def id_generator(size=4, chars='12345bcdefghijkmnoqrstuvwxyz'):
+    tmp = chars
+    value = ""
+    for _ in range(size):
+        t = random.choice(tmp)
+        value += t
+        tmp.replace(t, "")
+    print value
+    return value
---
+++ tlos_wallet.py
@@ -0,0 +1,124 @@
+import os
+import sys
+import json
+from time import sleep
+from tlos_utility import run
+from tlos_utility import get_output
+from tlos_utility import background
+from tlos_utility import start_background_proc
+from tlos_utility import log_file
+
+class tlos_wallet:
+    wallet_dir = ""
+    teclos_dir = ""
+    eos_dir = ""
+    keosd_dir = ""
+    wallet_address = 'http://127.0.0.1:8900'
+    teclos_start = ""
+    unlockTimeout = 0
+
+    def __init__(self, wallet_dir, teclos_dir, eos_dir, keosd_dir, unlockTimeout = 120):
+        self.wallet_dir = wallet_dir
+        self.teclos_dir = teclos_dir
+        self.eos_dir = eos_dir
+        self.keosd_dir = keosd_dir
+        self.teclos_start = "%s --wallet-url %s" % (teclos_dir, self.wallet_address)
+        self.unlockTimeout = unlockTimeout
+        if not self.exists() and os.path.isdir(eos_dir):
+            self.create()
+
+    def exists(self):
+        return os.path.isdir(self.wallet_dir)
+
+    def create(self):
+        print "creating wallet"
+        self.start_wallet()
+        if not os.path.exists(self.wallet_dir):
+            os.makedirs(self.wallet_dir)
+        o = get_output(self.teclos_start + ' wallet create')
+        f = open(os.path.join(self.wallet_dir, 'wallet_pw.txt'), 'w')
+        f.write(self.parse_pw(o))
+
+    def unlock(self):
+        if not self.exists():
+            print 'No existing wallet, creating default wallet'
+            self.create()
+        elif self.is_locked():
+            run(self.teclos_start + ' wallet unlock --password ' + self.get_pw())
+            
+
+    def lock(self):
+        if not self.exists():
+            print 'No existing wallet, creating default wallet'
+            self.create()
+        elif not self.is_locked():
+            run(self.teclos_start + ' wallet lock')
+
+    def is_locked(self):
+        o = get_output(self.teclos_start + ' wallet list')
+        j = o[o.index(':') + 2:len(o)]
+        for wallet in j:
+            if 'default' in wallet and '*' in wallet:
+                return True
+        return False
+
+    def get_keys(self):
+        if self.is_locked():
+            self.unlock()
+        o = get_output(self.teclos_start + ' wallet private_keys --password %s' % self.get_pw())
+        j = json.loads(o)
+        output = []
+        for keypair in j:
+            d = {}
+            d['public'] = keypair[0]
+            d['private'] = keypair[1]
+            output.append(d)
+        return output
+
+    def contains_key(self, key):
+        if self.is_locked():
+            self.unlock()
+        o = get_output(self.teclos_start + ' wallet private_keys --password %s' % self.get_pw())
+        j = json.loads(o)
+        for keypair in j:
+            if key == keypair[0] or key == keypair[1]:
+                return True
+        return False
+
+    def create_key(self):
+        o =  filter(None, get_output(self.teclos_dir + ' create key').split("\n"))
+        private = o[0][o[0].index(':') + 2:len(o[0])]
+        public = o[1][o[1].index(':') + 2:len(o[1])]
+        return { "private" : private, "public" : public }
+
+    def import_key(self, private_key):
+        run(self.teclos_start + ' wallet import %s' % (private_key))
+
+    def parse_pw(self, o):
+        try:
+            if (o != "" or o != None) and 'Error' not in o:
+                r = o[o.index('"') + 1:len(o) - 2]
+                return r
+            else: 
+                print 'unable to parse wallet password'
+        except Exception as identifier:
+            print 'An error occurred while parsing password. Do you have a wallet?'
+            print identifier.message
+            sys.exit(1)
+
+    def start_wallet(self):
+        start_background_proc(self.keosd_dir + ' --unlock-timeout %d --http-server-address 127.0.0.1:8900' % (self.unlockTimeout), log_file('stderr.txt'))
+        sleep(.4)
+
+    def get_pw(self):
+        try:
+            p = os.path.join(self.wallet_dir, "wallet_pw.txt")
+            if os.path.exists(p):
+                f = open(p, 'r')
+                return f.readline()
+            else:
+                print "wallet password does not exist"
+        except Exception as identifier:
+            print 'An error occurred while attempting to get wallet password from ./wallet/wallet_pw.txt'
+            print identifier.message
+            sys.exit(1)
