cb32ff08de9188730f39d79386c1c338ef463030
ARoomWithABue <peter@hornofthemoon.com>
2018-07-19 16:54:30 -0700

    added a main execution test for asset

--- asset.py
+++ asset.py
@@ -1,7 +1,11 @@
 class Asset:
-    def __init__(self, amount, symbol = 'TLOS'):
+    def __init__(self, amount, symbol='TLOS'):
         self.symbol = symbol
         self.amount = amount
 
     def __str__(self):
         return ('%s %s') % (str.format(str(self.amount), '.4f'), self.symbol)
+
+if __name__ == '__main__':
+    a = Asset(1000)
+    print(a)
